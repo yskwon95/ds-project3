8d2b9448d370bb88b80918edf7d56c49c6c91f9d
yskwon95 <52399437+yskwon95@users.noreply.github.com>
2021-03-26 00:18:08 +0900

    asd

---
+++ config.py
@@ -0,0 +1,21 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user='nlclomzg',
+    pw='BIX18mbSMKRi2ZyEUm3cbg6Z5oc0Ail2',
+    url='rosie.db.elephantsql.com',
+    db='nlclomzg')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user='nlclomzg',
+    pw='BIX18mbSMKRi2ZyEUm3cbg6Z5oc0Ail2',
+    url='rosie.db.elephantsql.com',
+    db='nlclomzg')
